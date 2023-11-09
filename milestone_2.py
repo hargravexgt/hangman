eedff3ff04073c4226625168cabaa1454870340f
hargravexgt <georgehargrave11@me.com>
2023-11-09 08:25:23 +0200

    Creating milestone_2.py

---
+++ milestone_2.py
@@ -0,0 +1,9 @@
+import random
+
+word_list = ["Apple", "Kiwi", "Banana", "Watermelon", "Mango"]
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input("What's your guess?")

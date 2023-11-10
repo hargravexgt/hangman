6ae4397e649477b293e691f871ae9dba8d092a31
hargravexgt <georgehargrave11@me.com>
2023-11-10 07:29:33 +0200

    Bringing up to date

--- milestone_3.py
+++ milestone_3.py
@@ -2,13 +2,25 @@ import milestone_2
 
 word = milestone_2.random_word()
 
-result = None
+def check_in_word(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess: {guess} is in the word")
+        return True
+    else:
+        print(f"Sorry, {guess} not in the word. Try again!")
+        return False
 
-while True:
-    if bool(result) == True:
-        print(result)
-        break
-    guess = milestone_2.ask_for_guess()
-    result = milestone_2.check_guess(guess)
+def ask_for_input():
+    result = None
+    while True:
+        if bool(result) == True:
+            print(result)
+            break
+        guess = milestone_2.ask_for_guess()
+        result = milestone_2.check_valid_guess(guess)
+    check_in_word(guess)
+    return guess
 
-print(guess)
+
+guess = ask_for_input()
